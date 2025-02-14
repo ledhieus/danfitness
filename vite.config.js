faa74f335e31db1cb4c7ee05f352f4975d72b060
ledhieus <ledan0602@gmail.com>
2025-02-14 20:32:04 +0700

    vercel

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Đảm bảo React Router xử lý route
+  }
 })
